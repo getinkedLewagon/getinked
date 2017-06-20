8c450cbc29687a3851d22a81eda9b6a8c513f6e1
Courtney Treweek <c.k.treweek@gmail.com>
2017-06-20 15:02:38 +0200

    pages controller home and dashboard methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @locations = Artist.all.map {|a| a.location}.uniq
   end
 
   def dashboard
+    @locations = Artist.all.map {|a| a.location}.uniq
+    @appointments = current_user.appointments
   end
+
 end
