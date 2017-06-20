4c95d2973fda3a35396e917a18e6c99f180d5bf8
Oleg Jakunin <oleg.kemz@gmail.com>
2017-06-20 17:59:24 +0200

    i've done something kinda

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @artist = Artist.find(params[:id])
     @locations = Artist.all.map {|a| a.location}.uniq
   end
 
   def dashboard
     @locations = Artist.all.map {|a| a.location}.uniq
-    @appointments = current_user.appointments
+    if current_user
+      @appointments = current_user.appointments
+    else
+      @appointments = current_artist.appointments
+    end
   end
-
 end
