b468cd6ea14c0fb0610b7df261e4dfd3093dd559
Lauren DeFrancesco <lauren209@live.com>
2017-06-20 14:23:56 +0200

    made a basic artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
+    @appointment = Appointment.new
   end
 end
