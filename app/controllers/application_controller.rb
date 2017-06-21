0f013aa3bcc9adc5e02cb7fac6dd7f5a96e114b5
Lauren DeFrancesco <lauren209@live.com>
2017-06-21 18:36:52 +0200

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :info, :city])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 #   def after_sign_in_path_for(resource)
--- db/migrate/20170621145245_add_start_date_and_end_date_to_appointments.rb
+++
@@ -1,6 +0,0 @@
-class AddStartDateAndEndDateToAppointments < ActiveRecord::Migration[5.0]
-  def change
-    add_column :appointments, :start_time, :string
-    add_column :appointments, :end_time, :string
-  end
-end
