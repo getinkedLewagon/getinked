c6a075c64d51070e06baf558d5f26664b7e9ec1b
Courtney Treweek <c.k.treweek@gmail.com>
2017-06-23 16:08:29 +0200

    confirm and deny appointments

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :artists, controllers: { registrations: "artists/registrations",
     omniauth_callbacks: 'artists/omniauth_callbacks' }
-  devise_for :users
+    devise_for :users
 
-  root to: 'pages#home'
+    root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/about', to: 'pages#about'
   get '/dashboard', to: 'pages#dashboard'
@@ -13,17 +13,21 @@ Rails.application.routes.draw do
   resources :artists, only: [:index, :show, :edit, :update] do
 
    get '/dashboard/refresh', to: 'pages#refresh'
-  post '/dashboard', to: 'pages#create_message', as: "post_message"
+   post '/dashboard', to: 'pages#create_message', as: "post_message"
 
 
+   member do
+    post "upload_pictures", to: "artists#upload"
+  end
+  resources :reviews, only: [:new, :create]
+  resources :appointments, only: [:new, :create, :destroy, :show] do
     member do
-      post "upload_pictures", to: "artists#upload"
+      get 'confirm'
+      get 'deny'
     end
-    resources :reviews, only: [:new, :create]
-    resources :appointments, only: [:new, :create, :destroy, :show] do
       resources :messages, only: [:new, :create, :show]
     end
   end
-    resources :availabilities, only: [:new, :create, :destroy]
+  resources :availabilities, only: [:new, :create, :destroy]
 
 end
