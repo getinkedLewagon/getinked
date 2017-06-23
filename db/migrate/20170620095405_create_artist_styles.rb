ad5b35f962d2f2aa89f721d1147050f6aae8cd4a
Lauren DeFrancesco <lauren209@live.com>
2017-06-23 15:11:21 +0200

    Commentend a migration

--- db/migrate/20170620095405_create_artist_styles.rb
+++ db/migrate/20170620095405_create_artist_styles.rb
@@ -1,10 +1,10 @@
 class CreateArtistStyles < ActiveRecord::Migration[5.0]
   def change
     create_table :artist_styles do |t|
-      t.references :styles, foreign_key: true
-      t.references :artist, foreign_key: true
+    #  t.references :styles, foreign_key: true
+    #  t.references :artist, foreign_key: true
 
-      t.timestamps
+    #  t.timestamps
     end
   end
 end
