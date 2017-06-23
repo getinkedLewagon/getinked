72fda042f379cebbb27992e9a95e2420343affd9
Lauren DeFrancesco <lauren209@live.com>
2017-06-23 15:05:14 +0200

    Removed artist style

--- db/migrate/20170623130131_remove_style_from_artists.rb
+++ db/migrate/20170623130131_remove_style_from_artists.rb
@@ -1,4 +1,5 @@
 class RemoveStyleFromArtists < ActiveRecord::Migration[5.0]
   def change
+    remove_column :artists, :style
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623112311) do
+ActiveRecord::Schema.define(version: 20170623130131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 20170623112311) do
     t.datetime "token_expiry"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "style"
     t.index ["email"], name: "index_artists_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true, using: :btree
   end
