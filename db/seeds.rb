836598d12fe3c0a281ef030def837684a6634740
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-24 20:08:37 +0200

    Style system generated!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #
 # Examples:
 #
@@ -5,93 +6,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Photo.destroy_all
-Style.destroy_all
 ArtistStyle.destroy_all
+Style.destroy_all
 Artist.destroy_all
 
 User.destroy_all
 
-
-
-a_array = []
-
-    a = Artist.create({
-    name: "Rob Kelly",
-    info: "I do out of this world tattoos specializing in rocketships and birds",
-    start_fee: 100,
-    rating: nil,
-    city: "Barcelona",
-    email: "first@gmail.com",
-    password: "password1",
-    address: "Carrer Tallers, 29, 08001 Barcelona"
-    })
-
- a_array << a
-
-        a_array << Artist.create({
-    name: "John Smith",
-    info: "I do out of this world tattoos specializing in people faces",
-    start_fee: 100,
-    rating: nil,
-    city: "Paris",
-    email: "john@gmail.com",
-    password: "john@gmail.com",
-    address: "Carrer Tallers, 29, 08001 Barcelona"
-    })
-
-
-a_array << Artist.create({
-    name: "Doug Hardy",
-    info: "I have been tattooing for over a decade. During that time, I have mastered and refined my own take on classic American street-shop styles",
-    start_fee: 50,
-    rating: nil,
-    city: "Rome",
-    email: "second@gmail.com",
-    password: "password2",
-    address: "Via del Moro, 12, 00153 Rome"
-    })
-a_array << Artist.create({
-    name: "Angelique Houtkamp",
-    info: "I do out of this world tattoos specializing in horses and dogs",
-    start_fee: 45,
-    rating: nil,
-    city: "Barcelona",
-    email: "third@gmail.com",
-    password: "password3",
-    address: "Carrer del Comte d'Urgell, 82, 08036 Barcelona"
-    })
-
-p "Artists Created"
-
-p a_array
-
-# u_array = []
-
-# u_array << User.create({
-#   email: "userone@live.com",
-#   encrypted_password: "userpassword1",
-#   username: "USER1"
-#   })
-# u_array << User.create({
-#   email: "usertwo@live.com",
-#   encrypted_password: "userpassword2",
-#   username: "USER2"
-#   })
-# u_array << User.create({
-#   email: "userthree@live.com",
-#   encrypted_password: "userpassword3",
-#   username: "USER3"
-#   })
-
-
-
-
 styles = []
 
-
-
-
-styles << Style.create!({
+styles << Style.create({
   name: "Traditional"
   })
 styles << Style.create({
@@ -99,9 +22,6 @@ styles << Style.create({
   })
 styles << Style.create({
   name: "Realism"
-  })
-styles << Style.create({
-  name: "Tribal"
   })
 styles << Style.create({
   name: "New School"
@@ -112,67 +32,126 @@ styles << Style.create({
 styles << Style.create({
   name: "Japanese"
   })
-styles << Style.create({
-  name: "Dotwork"
-  })
-styles << Style.create({
-  name: "Geometric"
-  })
-styles << Style.create({
-  name: "Script"
-  })
-styles << Style.create({
-  name: "Illustrative"
-  })
+# styles << Style.create({
+#   name: "Dotwork"
+#   })
+# styles << Style.create({
+#   name: "Geometric"
+#   })
+# styles << Style.create({
+#   name: "Script"
+#   })
+# styles << Style.create({
+#   name: "Illustrative"
+#   })
+
+puts "Styles Created!"
+
+# Arrays for random assignment
+cities_array = ["Barcelona", "Rome", "Berlin", "Paris", "London"]
+start_fee_array = [60, 65, 70, 75, 80, 85, 90, 95, 100]
+
+# Generate Traditional Tattooers from Instagram (9)
+traditional_artist_array = ['dennis_gutierrez', 'fabriziomele', 'gordoncombs',
+                            'joeellistattoo', 'pauldobleman',
+                            'rafadecraneo', 'vinzflag', 'xcyrx']
+
+# Generate Traditional Tattooers from Instagram (8)
+
+realism_artist_array = ['megan_massacre', 'sununala', 'rafaramostattoo',
+                          'mills06', 'trashcore', 'zakkhanattattoo', 'lizvnenom',
+                          'mariotoloza_']
+
+# Fetch New School Tattooers from Instagram (5)
+
+new_school_artist_array = ['laurent_lajeunesse', 'debbieripper_tattoo', 'loxiput_tattoo',
+                            'wendellyap', 'austinzfoo']
+
+# Fetch Neo Traditional Tattooers from Instagram (7)
+
+neo_traditional_artist_array = ['shio1red', 'carlykroll', 'haydntattoo', 'samanthamoth13',
+                                'causalitytattoo', 'motar_ink', 'jptattooartist']
+
+# Fetch Japanese Tattooers from Instagram (7)
+
+japanese_artist_array = ['japanesetattoo', 'horishige_5', 'elliottwells666', 'brinditattoo',
+                          'larascotton', 'stueysingh', ]
+
+# dotwork_artist_array = []
+
+# geometric_artist_array = []
+
+# script_artist_array = []
+
+# illustrative_artist_array = []
+
+
+def artist_full(username)
+  url = "https://hashtagbattle.com/v1/instagram/users/#{username}?token=754673ab54bcde3245
+"
+  parse = JSON.parse(open(url).read)
+end
+
+def artist_data(parse)
+  extract_user_data = parse["entry_data"]["ProfilePage"][0]["user"]
+end
+
+def artist_pictures(parse)
+  extract_user_pictures = parse["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"]
+end
+
+# User on each of the nodes elements to extract picture url [0]["display_src"]
+
+traditional_artist_array.each do |username|
+  full_info = artist_full(username)
+  profile = artist_data(full_info)
+  pictures = artist_pictures(full_info)
+
+  new_artist = Artist.new
+  new_artist.city = cities_array.sample
+  new_artist.name = profile["full_name"]
+  new_artist.info = profile["biography"]
+  new_artist.email = "#{username}@getinked.com"
+  new_artist.styles = [styles[0]]
+  new_artist.start_fee = start_fee_array.sample
+  new_artist.save
+
+  puts "#{username} saved as an artist!"
+
+  pictures.each do |picture|
+    photo = Photo.new
+    photo.artist = new_artist
+    photo.url = picture["display_src"]
+    photo.save
+    puts "    photo saved for #{username}!"
+  end
+
+
+end
 
 
 
-style_id = styles.sample
-
-ArtistStyle.create({
-  style_id: style_id.id,
-  artist_id: a_array[0].id
-  })
-
-ArtistStyle.create({
-  style_id: style_id.id,
-  artist_id: a_array[1].id
-  })
-
-ArtistStyle.create({
-  style_id: style_id.id,
-  artist_id: a_array[2].id
-  })
-
-
-Photo.create({
-  url: "https://68.media.tumblr.com/7be6d31baa529a275d17f2fea9c37ea7/tumblr_oomf7lm9um1r4vezwo1_500.jpg",
-  artist_id: a_array[1].id
-  })
-
-Photo.create({
-  url: "https://68.media.tumblr.com/b0670ae6949ddc4230370c02c59a2976/tumblr_oa9zu5nOO41v34nkno1_500.jpg",
-  artist_id: a_array[1].id
-  })
-
-Photo.create({
-  url: "https://68.media.tumblr.com/fbf63393b31e6f479ef65c67f469217e/tumblr_o5u692nWHV1v34nkno1_500.jpg",
-  artist_id: a_array[0].id
-  })
-
-Photo.create({
-  url: "https://s-media-cache-ak0.pinimg.com/originals/b7/97/31/b7973143d630156af17f9eb823ca26d6.jpg",
-  artist_id: a_array[2].id
-  })
-
-Photo.create({
-  url: "https://68.media.tumblr.com/943a3b217dde91527df77bc77a466971/tumblr_ok4ost40hv1r4vezwo1_500.jpg",
-  artist_id: a_array[3].id
-  })
-
-p "Photos Created"
 
 
 
 
-p "Styles Created"
+
+
+
+# Generate Watercolor   Tattooers from Instagram
+# Generate Realism Tattooers from Instagram
+# Generate Tribal Tattooers from Instagram
+# Generate New School Tattooers from Instagram
+# Generate Neo Traditional Tattooers from Instagram
+# Generate Japanese Tattooers from Instagram
+# Generate Dotwork Tattooers from Instagram
+# Generate Geometric Tattooers from Instagram
+# Generate Script Tattooers from Instagram
+# Generate Illustrative Tattooers from Instagram
+
+
+
+
+
+
+
