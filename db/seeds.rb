c3918322220bc2faf323528549a4482e8bd061e8
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-24 21:27:20 +0200

    seeding method refactored to accept hashes instead of arrays

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,26 +12,29 @@ Artist.destroy_all
 
 User.destroy_all
 
-styles = []
 
-styles << Style.create({
-  name: "Traditional"
-  })
-styles << Style.create({
-  name: "Watercolor"
-  })
-styles << Style.create({
-  name: "Realism"
-  })
-styles << Style.create({
-  name: "New School"
-  })
-styles << Style.create({
-  name: "Neo Traditional"
-  })
-styles << Style.create({
-  name: "Japanese"
-  })
+  styles = []
+
+  styles << Style.create({
+    name: "Traditional"
+    })
+  styles << Style.create({
+    name: "Watercolor"
+    })
+  styles << Style.create({
+    name: "Realism"
+    })
+  styles << Style.create({
+    name: "New School"
+    })
+  styles << Style.create({
+    name: "Neo Traditional"
+    })
+  styles << Style.create({
+    name: "Japanese"
+    })
+
+
 # styles << Style.create({
 #   name: "Dotwork"
 #   })
@@ -45,37 +48,45 @@ styles << Style.create({
 #   name: "Illustrative"
 #   })
 
-puts "Styles Created!"
 
 # Arrays for random assignment
-cities_array = ["Barcelona", "Rome", "Berlin", "Paris", "London"]
-start_fee_array = [60, 65, 70, 75, 80, 85, 90, 95, 100]
+def cities_array
+  cities_array = ["Barcelona", "Rome", "Berlin", "Paris", "London"]
+end
 
+def start_fee_array
+  start_fee_array = [60, 65, 70, 75, 80, 85, 90, 95, 100]
+end
 # Generate Traditional Tattooers from Instagram (9)
-traditional_artist_array = ['dennis_gutierrez', 'fabriziomele', 'gordoncombs',
-                            'joeellistattoo', 'pauldobleman',
-                            'rafadecraneo', 'vinzflag', 'xcyrx']
+traditional_artist_hash = { style: Style.find_by(name: "Traditional"),
+                          artists: ['dennis_gutierrez', 'fabriziomele', 'gordoncombs',
+                                    'joeellistattoo', 'pauldobleman',
+                                    'rafadecraneo', 'vinzflag', 'xcyrx'] }
 
 # Generate Traditional Tattooers from Instagram (8)
 
-realism_artist_array = ['megan_massacre', 'sununala', 'rafaramostattoo',
-                          'mills06', 'trashcore', 'zakkhanattattoo', 'lizvnenom',
-                          'mariotoloza_']
+realism_artist_array = { style: Style.find_by(name: "Realism"),
+                         artists: ['megan_massacre', 'sununala', 'rafaramostattoo',
+                                  'mills06', 'trashcore', 'zakkhanattattoo', 'lizvnenom',
+                                  'mariotoloza_'] }
 
 # Fetch New School Tattooers from Instagram (5)
 
-new_school_artist_array = ['laurent_lajeunesse', 'debbieripper_tattoo', 'loxiput_tattoo',
-                            'wendellyap', 'austinzfoo']
+new_school_artist_array = { style: Style.find_by(name: "New School")
+                          artists: ['laurent_lajeunesse', 'debbieripper_tattoo', 'loxiput_tattoo',
+                                    'wendellyap', 'austinzfoo']
 
-# Fetch Neo Traditional Tattooers from Instagram (7)
+# F }etch Neo Traditional Tattooers from Instagram (7)
 
-neo_traditional_artist_array = ['shio1red', 'carlykroll', 'haydntattoo', 'samanthamoth13',
-                                'causalitytattoo', 'motar_ink', 'jptattooartist']
+neo_traditional_artist_array = { style: Style.find_by(name: "Neo Traditional")
+                                 artists: ['shio1red', 'carlykroll', 'haydntattoo', 'samanthamoth13',
+                                           'causalitytattoo', 'motar_ink', 'jpta }ttooartist']
 
 # Fetch Japanese Tattooers from Instagram (7)
 
-japanese_artist_array = ['japanesetattoo', 'horishige_5', 'elliottwells666', 'brinditattoo',
-                          'larascotton', 'stueysingh', ]
+japanese_artist_array = { style: Style.find_by(name: "Japanese")
+                          artists: ['japanesetattoo', 'horishige_5', 'elliottwells666', 'brinditattoo',
+                                    'larascotton', 'stueysingh', ] }
 
 # dotwork_artist_array = []
 
@@ -87,8 +98,7 @@ japanese_artist_array = ['japanesetattoo', 'horishige_5', 'elliottwells666', 'br
 
 
 def artist_full(username)
-  url = "https://hashtagbattle.com/v1/instagram/users/#{username}?token=754673ab54bcde3245
-"
+  url = "https://hashtagbattle.com/v1/instagram/users/#{username}?token=754673ab54bcde3245"
   parse = JSON.parse(open(url).read)
 end
 
@@ -102,32 +112,37 @@ end
 
 # User on each of the nodes elements to extract picture url [0]["display_src"]
 
-traditional_artist_array.each do |username|
-  full_info = artist_full(username)
-  profile = artist_data(full_info)
-  pictures = artist_pictures(full_info)
 
-  new_artist = Artist.new
-  new_artist.city = cities_array.sample
-  new_artist.name = profile["full_name"]
-  new_artist.info = profile["biography"]
-  new_artist.email = "#{username}@getinked.com"
-  new_artist.styles = [styles[0]]
-  new_artist.start_fee = start_fee_array.sample
-  new_artist.save
+def populate_style(style_hash)
+  style_hash[:artists].each do |username|
+    binding.pry
+    full_info = artist_full(username)
 
-  puts "#{username} saved as an artist!"
+    profile = artist_data(full_info)
+    pictures = artist_pictures(full_info)
 
-  pictures.each do |picture|
-    photo = Photo.new
-    photo.artist = new_artist
-    photo.url = picture["display_src"]
-    photo.save
-    puts "    photo saved for #{username}!"
+    new_artist = Artist.new
+    new_artist.city = cities_array.sample
+    new_artist.name = profile["full_name"]
+    new_artist.info = profile["biography"]
+    new_artist.email = "#{username}@getinked.com"
+    new_artist.styles = style_hash[:style]
+    new_artist.start_fee = start_fee_array.sample
+    new_artist.save
+
+    puts "#{username} saved as an artist!"
+
+    pictures.each do |picture|
+      photo = Photo.new
+      photo.artist = new_artist
+      photo.url = picture["display_src"]
+      photo.save
+      puts "    photo saved for #{username}!"
+    end
   end
-
-
 end
+
+populate_style(traditional_artist_hash)
 
 
 
