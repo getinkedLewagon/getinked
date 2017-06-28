eff7bc41e1f5a41bccd373330a480471b329cf2a
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-28 23:49:23 +0200

    material design form implemented on sign up page

--- app/controllers/artists/registrations_controller.rb
+++ app/controllers/artists/registrations_controller.rb
@@ -10,10 +10,12 @@ class Artists::RegistrationsController < Devise::RegistrationsController
 
     super
     @styles = params[:artist][:style]
-    @styles.delete_at(0)
-    @styles.each do |style|
+    if @styles
+      @styles.delete_at(0)
+      @styles.each do |style|
       style_m = Style.find(style)
       ArtistStyle.create(artist: current_artist, style: style_m)
+      end
     end
 
   end
