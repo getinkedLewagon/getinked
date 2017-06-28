99094121af809beeb94c27ef44615cfaf346d0de
Lauren DeFrancesco <lauren209@live.com>
2017-06-28 13:23:47 +0200

    -allow-empty-message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Photo.destroy_all
-# ArtistStyle.destroy_all
-# Style.destroy_all
- Artist.destroy_all
+ Photo.destroy_all
+ ArtistStyle.destroy_all
+ Style.destroy_all
+# Artist.destroy_all
 
-#Review.destroy_all
-# Chatroom.destroy_all
-# Message.destroy_all
-# User.destroy_all
-# Appointment.destroy_all
-# Availability.destroy_all
-# Order.destroy_all
-# Photo.destroy_all
+Review.destroy_all
+ Chatroom.destroy_all
+ Message.destroy_all
+ User.destroy_all
+ Appointment.destroy_all
+ Availability.destroy_all
+ Order.destroy_all
+ Photo.destroy_all
 
 
 #   styles = []
