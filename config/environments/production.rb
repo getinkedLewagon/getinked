f1a760b07101067407be3c7e5c19afbcff41e670
Lauren DeFrancesco <lauren209@live.com>
2017-06-28 15:19:02 +0200

    Adding cable stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = 'wss://getinked.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://getinked.herokuapp.com', 'http://getinked.herokuapp.com']
 end
