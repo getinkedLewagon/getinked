c0dc4bef72d686e331e90da03861efd7c78cd4f5
Oleg Jakunin <oleg.kemz@gmail.com>
2017-06-29 12:02:25 +0200

    some fancy animation

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,5 @@
-window.sr = ScrollReveal();
-sr.reveal('.info-padding');
-
+window.sr = ScrollReveal({distance: '30px'});
+// sr.reveal('.info-padding');
+sr.reveal('.paragraph-reveal', {duration: 1000});
+sr.reveal('.featured-artist-card', {duration: 1000, delay: 200});
+sr.reveal('.no-gutter-artist-index');
